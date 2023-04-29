8e82a4912e17bd4b7ecb0bee07ad5db5cd42971c
Stefan Babic <stefanbabic37@gmail.com>
2023-04-29 22:09:28 +0200

    Putanja za login i mapiranje za api putanje

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use App\Http\Controllers\Api\MovieController;
+use App\Http\Controllers\Api\ProjectionController;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +19,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/login', function (Request $request) {
+    $user = User::where('email', $request->email)->first();
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken($request->email)->plainTextToken;
+});
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::apiResources([
+        '/movies'=>MovieController::class,
+        '/projections'=>ProjectionController::class,
+    ]);
 });
